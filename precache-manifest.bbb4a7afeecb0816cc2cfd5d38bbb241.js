7600dea5a129cbb6c05b6acb0688cfcb55b9bcb9
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-04-22 20:58:34 +0200

    Updates

---
+++ precache-manifest.bbb4a7afeecb0816cc2cfd5d38bbb241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e2b6e3bf7b7255cf0c6",
+    "url": "/api-quote/static/js/runtime~main.334a494c.js"
+  },
+  {
+    "revision": "ed589c098794366ce328",
+    "url": "/api-quote/static/js/main.9addce1c.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/api-quote/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ed589c098794366ce328",
+    "url": "/api-quote/static/css/main.c4598673.chunk.css"
+  },
+  {
+    "revision": "3111cfce368b421d100f81174edfee7d",
+    "url": "/api-quote/index.html"
+  }
+];
